9a6ec66d4e2b59eedf693ec880347b80518d4709
sariah-shoe <85853241+sariah-shoe@users.noreply.github.com>
2022-01-29 11:55:20 -0700

    Update jamf_backup_apps.py

--- jamf_backup/jamf_backup_apps.py
+++ jamf_backup/jamf_backup_apps.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2021 Brandon Childers and Sariah Shoemaker
+# Copyright (c) 2021 Sariah Shoemaker
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
